c0175a74e5a5c17d838e531d37c71354efff59aa
Codersxyz <surajburkule20000@gmail.com>
2023-02-14 19:39:15 +0530

    ocr

--- proj_/android/app/src/main/kotlin/com/example/proj_exh/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.proj_exh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- proj_/lib/routes.dart
+++
@@ -1,8 +0,0 @@
-class MyRoutes {
-  static String homeroute = "/";
-  static String compressorroute = "/compressor";
-  static String scannerroute = "/scanner";
-  static String webservicesroute = "/webservices";
-  static String loginpageroute = "/loginpage";
-  static String pdfpageroute = "/pdf_page";
-}
